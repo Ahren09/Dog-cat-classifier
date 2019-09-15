124fbea521ca32b960a0c57b7c14d988e9ded0c9
Ahren09 <1099052927@qq.com>
2019-09-15 11:56:26 +0800

    Modify ResNet

--- models/resnet.py
+++ models/resnet.py
@@ -1,77 +1,85 @@
 # coding:utf8
 from .basic_module import BasicModule
-import torch
-import torch.nn as nn
+from torch import nn
 from torch.nn import functional as F
 
+
 class ResidualBlock(nn.Module):
-    def __init__(self, in_channels, out_channels, stride=1, shortcut=None):
+    """
+    实现子module: Residual Block
+    """
+
+    def __init__(self, inchannel, outchannel, stride=1, shortcut=None):
         super(ResidualBlock, self).__init__()
-        self.basic = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
+        self.left = nn.Sequential(
+            nn.Conv2d(inchannel, outchannel, 3, stride, 1, bias=False),
+            nn.BatchNorm2d(outchannel),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(out_channels),
-        )
-        self.shortcut = shortcut
+            nn.Conv2d(outchannel, outchannel, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(outchannel))
+        self.right = shortcut
 
     def forward(self, x):
-        out = self.basic(x)
-        residual = x
-        if self.shortcut is not None:
-            x = self.shortcut(x)
+        out = self.left(x)
+        residual = x if self.right is None else self.right(x)
         out += residual
         return F.relu(out)
 
 
 class ResNet(BasicModule):
+    """
+    实现主module：ResNet34
+    ResNet34包含多个layer，每个layer又包含多个Residual block
+    用子module来实现Residual block，用_make_layer函数来实现layer
+    """
+
     def __init__(self, num_classes=2):
         super(ResNet, self).__init__()
         self.model_name = 'resnet'
 
+        # 前几层: 图像转换
         self.pre = nn.Sequential(
             nn.Conv2d(3, 64, 7, 2, 3, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(3, 2, 1),
-        )
-        self.layer1 = self.make_layer(64, 128, 3)
-        self.layer2 = self.make_layer(128, 256, 2, stride=2)
-        self.layer3 = self.make_layer(256, 512, 2, stride=2)
-        self.layer4 = self.make_layer(512, 512, 2, stride=2)
+            nn.MaxPool2d(3, 2, 1))
 
+        # 重复的layer，分别有3，4，6，3个residual block
+        self.layer1 = self._make_layer(64, 128, 3)
+        self.layer2 = self._make_layer(128, 256, 4, stride=2)
+        self.layer3 = self._make_layer(256, 512, 6, stride=2)
+        self.layer4 = self._make_layer(512, 512, 3, stride=2)
+
+        # 分类用的全连接
         self.fc = nn.Linear(512, num_classes)
 
-    def make_layer(self, in_channels, out_channels, block_num, stride=1):
+    def _make_layer(self, inchannel, outchannel, block_num, stride=1):
+        """
+        构建layer,包含多个residual block
+        """
         shortcut = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 1, stride, bias=False), # kernel size=1
-            nn.BatchNorm2d(out_channels),
-        )
-        layers = []
+            nn.Conv2d(inchannel, outchannel, 1, stride, bias=False),
+            nn.BatchNorm2d(outchannel))
 
-        # There are block_num layers Residual Blocks stacked together.
-        # The input has different number of layers from output
-        layers.append(ResidualBlock(in_channels, out_channels, stride, shortcut))
-        for i in range(1, block_num): # TODO: Why range(block_num-1) not working?
-            layers.append(ResidualBlock(out_channels, out_channels)) # stride=1
+        layers = []
+        layers.append(ResidualBlock(inchannel, outchannel, stride, shortcut))
+
+        for i in range(1, block_num):
+            layers.append(ResidualBlock(outchannel, outchannel))
         return nn.Sequential(*layers)
 
     def forward(self, x):
         x = self.pre(x)
+        print("Layer1")
         x = self.layer1(x)
-        print("After 1")
+        print("Layer2")
         x = self.layer2(x)
-        print("After 2")
+        print("Layer3")
         x = self.layer3(x)
-        print("After 3")
+        print("Layer4")
         x = self.layer4(x)
-        print("After 4")
-        print(x.shape)
-        x = F.avg_pool2d(x, 7, stride=1)
-        print(x.shape)
+        print("AvgPool")
+
+        x = F.avg_pool2d(x, 7)
         x = x.view(x.size(0), -1)
-        print(x.shape)
         return self.fc(x)
-
-
